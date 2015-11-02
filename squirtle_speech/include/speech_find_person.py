209b6919259efbfee98030f77373c27c7e4585bf
siddharth <srsidd@bitbucket.org>
2015-11-02 01:54:06 -0500

    Adding package for the speech stuff on Squirtle. Includes scripts for playing voice commands and ros node to monitor status of the task

---
+++ squirtle_speech/include/speech_find_person.py
@@ -0,0 +1,24 @@
+import pyttsx
+import sys
+
+if len(sys.argv)<2:
+	print 'usage: python script.py [person] '
+	person = 'David'
+else:
+	person = str(sys.argv[1])
+
+print 'Name: ',person
+
+engine = pyttsx.init()
+engine.setProperty('rate', 150)
+
+voices = engine.getProperty('voices')
+num = 16
+
+engine.setProperty('voice', voices[num].id)
+
+speechstring = "Hey, "+person+"! Come over here."
+engine.say(speechstring)
+
+engine.runAndWait()
+
