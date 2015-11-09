be15193fec9ad4b095d8357a928660b1eeb6e1db
srsidd <srsidd@seas.upenn.edu>
2015-11-09 10:54:53 -0500

    Added code to kill the destination point node

--- squirtle_navigation/include/NavigationKillNodes.sh
+++ squirtle_navigation/include/NavigationKillNodes.sh
@@ -1,7 +1,5 @@
 rosnode kill map_server
 sleep 1
-rosnode kill amcl
-sleep 1
 rosnode kill laser_filter
 sleep 1
 rosnode kill laser_tf_publisher
@@ -12,7 +10,11 @@ rosnode kill kobuki_safety_controller
 sleep 1
 rosnode kill navigation_velocity_smoother
 sleep 1
+rosnode kill amcl
+sleep 1
 rosnode kill move_base
 sleep 1
 rosnode kill subsroutineStatus
+sleep 1
+rosnode kill destination_point
 sleep 1
