4a60e1e7c22c3402912c656b0d6ef24ce7b166b8
vincentwenxuan <vincentwenxuan@gmail.com>
2015-12-07 14:53:21 -0500

    mouth package can handle recognizer now

--- wenxuan_ws/wenxuanbot_voice/squirtle_mouth/scripts/mouth_talker.py
+++ wenxuan_ws/wenxuanbot_voice/squirtle_mouth/scripts/mouth_talker.py
@@ -5,6 +5,8 @@ import rospy
 #from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
 from std_msgs.msg import String
+import rospy.service
+import std_srvs.srv
 
 
 class Mouth_talker:
@@ -22,11 +24,35 @@ class Mouth_talker:
         self.voice = 'voice_cmu_us_rms_arctic_clunits'
         rospy.loginfo("Mouth initialize complete.")
 
+        # recognizer service, note that the recognizer may not exist
+        self.recognizer_stop = rospy.ServiceProxy('/recognizer/stop', std_srvs.srv.Empty)
+        self.recognizer_start = rospy.ServiceProxy('/recognizer/start', std_srvs.srv.Empty)
+
+        
+
     def callback(self,msg):
         rospy.loginfo(rospy.get_caller_id() + ": I received %s", msg.data)
 
+
+        delay_length = len(msg.data)/13.0
+        try:
+            self.recognizer_stop()
+            rospy.loginfo("recognizer stopped")
+        except Exception, e:
+            rospy.loginfo("no recognizer to stop")
+
         self.soundhandle.say(msg.data, self.voice, 1.0)
-        
+
+        rospy.sleep(delay_length)
+
+        try:
+            self.recognizer_start()
+            rospy.loginfo("recognizer restarted")
+        except Exception, e:
+            rospy.loginfo("no recognizer to restart")
+
+   
+                
 
 
 if __name__ == '__main__':
