a12ff7bcc8234e7c8196bb8ee1242c3bbb54f722
srsidd <srsidd@seas.upenn.edu>
2015-12-14 11:13:07 -0500

    Bug fixes in interfacing tabletop detector with classifier

--- squirtle_object_detection/src/obj_detection_status.py
+++ squirtle_object_detection/src/obj_detection_status.py
@@ -12,36 +12,52 @@ Subscribes -
 import roslib
 import rospy
 import os
-
+from os import walk
+import sys
+from std_msgs.msg import *
 
 class subsroutinestatus:
 
 	def __init__(self, argument):
 		self.subRoutineStatus = 0
+		self.obj_to_detect = argument
 		# Setup the publishers and subscribers
 		rospy.init_node("objectDetectStatus", anonymous=True)
 		self.SubRoutineStatusPub = rospy.Publisher('current_subroutine_status', String, queue_size=10)
+		self.objectPub = rospy.Publisher('detected_object', String, queue_size=10)
+		os.system('rosrun opencv_files tabletop_detector &')
 		
-		os.system('rosrun opencv_files tabletop_detector')
-		rospy.Timer(rospy.Duration(10), object_callback)
+		rospy.Timer(rospy.Duration(10.0), self.object_callback)
+		self.SubRoutineStatusPub.publish("While") 
+		
 		while not rospy.is_shutdown():
 			# do nothing
-			pass
+				pass
 		rospy.spin()
 			
 
-	def object_callback():
+	def object_callback(self, event):
+		print "I killed and re-called tabletop"
 		os.system('/home/squirtle/catkin_ws/src/CIS700_Squirtle/squirtle_object_detection/include/kill_tabletop_detector.sh')
+		f = []
 		for (dirpath, dirnames, filenames) in walk("/home/squirtle/images"):
 			f.extend(filenames)
 			break
-
+			
+		print f
+		objects = []
 		for filename in f:
-			obj = os.system('python ~/Documents/caffe/python/workingTest.py ' + filename)
-			if(obj == argument):
-				print "Identified object is " + obj
-
-		os.system('rosrun opencv_files tabletop_detector')
+			obj = os.system('python /home/squirtle/Documents/caffe/python/testSVM.py /home/squirtle/images/' + filename)
+			#obj = obj.split('_')[0]
+			objects.append(obj)
+		if(self.obj_to_detect in objects):
+			print "Identified object is " + obj
+			self.objectPub(self.obj_to_detect)
+		else:
+			print " I did not find the object :("	
+			self.objectPub("no obj")
+		os.system('rosrun opencv_files tabletop_detector &')
+		self.SubRoutineStatusPub.publish("Callback") 
 		return
 
 
